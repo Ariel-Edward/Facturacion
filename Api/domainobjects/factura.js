ed239cec121bdf0635dc63dc957fd880312540b6
Luis Vega <vsoliz.ariel@gmail.com>
2020-02-27 23:53:32 -0400

    aumentados otros argumentos para la creacion de la factura

--- Api/domainobjects/factura.js
+++ Api/domainobjects/factura.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Factura: function(persona, monto){
+    Factura: function(persona, monto, fecha, estado){
         this.persona = function(_persona) {
             if(!(_persona instanceof Persona)) {
                 throw new Error("Persona no permitida");
