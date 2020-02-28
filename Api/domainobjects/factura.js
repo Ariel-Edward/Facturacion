13277b4ec5956b174c56f0e6ad496cdc57bfe17c
Luis Vega <vsoliz.ariel@gmail.com>
2020-02-27 23:42:16 -0400

    construyendo clase factura

--- Api/domainobjects/factura.js
+++ Api/domainobjects/factura.js
@@ -1,6 +1,11 @@
 module.exports = {
     Factura: function(persona, monto){
-        
+        this.persona = function(_persona) {
+            if(!(_persona instanceof Persona)) {
+                throw new Error("Persona no permitida");
+            }
+            return _persona;
+        }(persona);
         
         Object.freeze(this);
     }
